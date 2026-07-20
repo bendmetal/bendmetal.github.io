949d9682cdba1c3db033da4f4d57f3de5dfe7a20
bendmetal <39422587+bendmetal@users.noreply.github.com>
2026-07-20 17:26:37 +0300

    Move risk disclaimer to top of landing page, under the title

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // EC225 Flight Tools Landing - Service Worker
 // Caches the landing page for offline use
 
-const CACHE_NAME = 'ec225-landing-v13';
+const CACHE_NAME = 'ec225-landing-v14';
 const FILES_TO_CACHE = [
   './',
   './index.html'
